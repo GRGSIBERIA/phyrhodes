c32daaf3611cefcaa421fa1f4c8ee2b2dc2b798c
grgsiberia <nanashi4129@gmail.com>
2019-10-16 19:14:11 +0900

    とても長い方程式になる

--- docs/paper/python/unfold_lagrange_T.py
+++ docs/paper/python/unfold_lagrange_T.py
@@ -1,37 +1,39 @@
 import sympy
-from sympy import symbols, cos, sin, diff
+from sympy import symbols, cos, sin, diff, Rational, latex, factor
 
 x = symbols("x_1 x_2 x_3 x_4 x_5 x_6")
 y = symbols("y_1 y_2 y_3 y_4 y_5 y_6")
 l = symbols("l_1 l_2 l_3 l_4 l_5 l_6")
-t = symbols("t_1 t_2 t_3 t_4 t_5 t_6")
+t = symbols("\\theta_1 \\theta_2 \\theta_3 \\theta_4 \\theta_5 \\theta_6")
+dt = symbols("\\dot{\\theta_1} \\dot{\\theta_2} \\dot{\\theta_3} \\dot{\\theta_4} \\dot{\\theta_5} \\dot{\\theta_6}")
 m = symbols("m_1 m_2 m_3 m_4 m_5 m_6")
 
 a = [l[i] * cos(t[i]) for i, _ in enumerate(x)]
 b = [l[i] * sin(t[i]) for i, _ in enumerate(x)]
 
-da = [-l[i] * t[i] * sin(t[i]) for i, _ in enumerate(x)]
-db = [l[i] * t[i] * cos(t[i]) for i, _ in enumerate(x)]
+da = [-l[i] * dt[i] * sin(t[i]) for i, _ in enumerate(x)]
+db = [ l[i] * dt[i] * cos(t[i]) for i, _ in enumerate(x)]
 
-x = [
-    a[0] + a[1] + a[2] + a[3],
-    a[1] + a[2] + a[3],
-    a[2] + a[3],
-    a[3],
-    a[4] + a[3],
-    a[5] + a[4] + a[3]
+dx = [
+    da[0] + da[1] + da[2] + da[3],
+    da[1] + da[2] + da[3],
+    da[2] + da[3],
+    da[3],
+    da[4] + da[3],
+    da[5] + da[4] + da[3]
 ]
 
-y = [
-    b[0] + b[1] + b[2] + b[3],
-    b[1] + b[2] + b[3],
-    b[2] + b[3],
-    b[3],
-    b[4] + b[3],
-    b[5] + b[4] + b[3]
+dy = [
+    db[0] + db[1] + db[2] + db[3],
+    db[1] + db[2] + db[3],
+    db[2] + db[3],
+    db[3],
+    db[4] + db[3],
+    db[5] + db[4] + db[3]
 ]
 
-
+T = [m[i] * (dx[i] ** 2 + dy[i] ** 2) for i, _ in enumerate(x)]
+print(latex(sum(T)))
 
 #
 #x1 = l1 * cos(t1) + l2 * cos(t2) + l3 * cos(t3) + l4 * cos(t4)
